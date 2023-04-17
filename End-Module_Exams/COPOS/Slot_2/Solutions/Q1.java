16e5c018c632c5f17fe2adec120bba7add437426
Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>
2023-04-17 19:54:13 +0530

    Update Q1.java
    Signed-off-by: Abdul Ahad Sheikh <95898825+gitabdulahad@users.noreply.github.com>

--- End-Module_Exams/COPOS/Slot_2/Solutions/Q1.java
+++ End-Module_Exams/COPOS/Slot_2/Solutions/Q1.java
@@ -4,23 +4,27 @@ public class arrays {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Enter the size of array");
 		int a[] = new int[sc.nextInt()];
-		//taking size as input from user
+		
+		// Taking size as input from user
+		
 		System.out.println("Enter the elements of array");
-		//taking elements as input
+		// Taking elements as input
 		for (int i = 0; i < a.length; i++) {
 			a[i] = sc.nextInt();
 		}
 
 		System.out.println("Elements of array: ");
-		//printing array elements
+		// Printing array elements
+		
 		for(int i=0; i<a.length; i++) {
 		    System.out.print(a[i]+" ");
 		}
 		System.out.println();
 		
-		//sorting array
+		// Sorting array
 		int temp = 0;
 		for (int i = 0; i < a.length; i++) {
 			for (int j = i + 1; j < a.length; j++) {
@@ -32,24 +36,31 @@ public class arrays {
 			}
 		}
 
-		int count = 0;//to count the frequency
-		int i = 0;//loop iterator
-		for (int j = i + 1; j < a.length; j++) {//traversing through the array
-			//track the just next element to i'th index
+		int count = 0;			// To count the frequency
+		int i = 0;			// Loop iterator
+		for (int j = i + 1; j < a.length; j++) 
+		{
+			// Traversing through the array
+			// Track the just next element to i'th index
 			if (a[i] == a[j]) {//if element matches then count++
 				count++;
 
-			} else {
+			} 
+			else 
+			{
 				System.out.println("frequency of element " + a[i] + " is " + (count + 1));
-				//print count+1 because there will be atleast one element
+				
+				// Print count+1 because there will be atleast one element
 				i = j;//reset the i'th index
+				
 				count = 0;//reset the count
 
 			}
 		}
+		
 		System.out.println("frequency of element " + a[a.length - 1] + " is " + (count + 1));
-		// the last element will be left untracked 
-		//so this statement is specially for last element
+		// The last element will be left untracked 
+		// So this statement is specially for last element
 
 	}
 
